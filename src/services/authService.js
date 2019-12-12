693efd2ded21fb363ca23afda4ea1162687cc6db
Eduardo Rodriguez <rodriguez.ambriz@outlook.com>
2019-12-12 12:02:29 -0600

    fixing little login issue and bi-directional dependencies

--- src/services/authService.js
+++ src/services/authService.js
@@ -5,31 +5,38 @@ import { apiUrl } from '../config.json';
 const apiEndpoint = `${apiUrl}/auth`;
 const tokenKey = 'token';
 
-export const login = async (email, password) => {
-  const { data: jwt } = http.post(apiEndpoint, { email, password });
-  localStorage.setItem(tokenKey, jwt);
-};
+http.setJwt(getJwt());
 
-export const loginWithJwt = jwt => {
+export async function login(email, password) {
+  const { data: jwt } = await http.post(apiEndpoint, { email, password });
   localStorage.setItem(tokenKey, jwt);
-};
+}
 
-export const logout = () => {
+export function loginWithJwt(jwt) {
+  localStorage.setItem(tokenKey, jwt);
+}
+
+export function logout() {
   localStorage.removeItem(tokenKey);
-};
+}
 
-export const getCurrentUser = () => {
+export function getCurrentUser() {
   try {
     const jwt = localStorage.getItem('token');
     return jwtDecode(jwt);
   } catch (ex) {
     return null;
   }
-};
+}
+
+export function getJwt() {
+  return localStorage.getItem(tokenKey);
+}
 
 export default {
   login,
   loginWithJwt,
   logout,
-  getCurrentUser
+  getCurrentUser,
+  getJwt
 };
